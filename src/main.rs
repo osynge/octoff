5a59736abc908114ca4d9087da5897eef16f01a7
Owen Synge <osynge@googlemail.com>
2020-07-28 09:41:50 +0200

    add clap and fern to project

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use chrono::{DateTime, Local, TimeZone};
 use huelib::resource::{light, Modifier, ModifierType};
 use huelib::{bridge, Bridge};
 use std::time::Duration;
+mod clap_fern;
+mod cli_clap;
 mod hue_ctrl;
 
 fn main() {
+    let clap_matches = cli_clap::cli_clap();
+    clap_fern::log_setup(&clap_matches);
+
     let fut_1 = hue_ctrl::fred();
     task::block_on(async {
         fut_1.await;
